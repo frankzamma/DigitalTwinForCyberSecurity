bd078b102fc463309838ecd2a3b3a4637213fb16
frankzamma <frank581.fgz@gmail.com>
2023-09-03 19:17:49 +0200

    Update della funzionalità Analisi durante la scrittura

--- src/main/java/it/unisa/zammarrelli/digitaltwinforcybersecurity/actions/WritingAnalysisHandler.java
+++ src/main/java/it/unisa/zammarrelli/digitaltwinforcybersecurity/actions/WritingAnalysisHandler.java
@@ -6,19 +6,23 @@ import com.intellij.codeInsight.editorActions.TypedHandlerDelegate;
 import com.intellij.notification.NotificationGroupManager;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.NotificationsManager;
+import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.LogicalPosition;
 import com.intellij.openapi.editor.actionSystem.TypedActionHandler;
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.psi.PsiFile;
 import com.theokanning.openai.OpenAiHttpException;
 import it.unisa.zammarrelli.digitaltwinforcybersecurity.ai.GPTWrapper;
 import it.unisa.zammarrelli.digitaltwinforcybersecurity.common.VulnerabilityLine;
+import it.unisa.zammarrelli.digitaltwinforcybersecurity.gui.PluginToolWindowLogWritingAnalysis;
 import it.unisa.zammarrelli.digitaltwinforcybersecurity.settings.PluginSettingsStateService;
 import org.jetbrains.annotations.NotNull;
 
@@ -39,30 +43,50 @@ public class WritingAnalysisHandler extends TypedHandlerDelegate {
                 int start = document.getLineStartOffset(logicalPosition.line-1);
                 int end = document.getLineEndOffset(logicalPosition.line -1);
 
-                String line = editor.getDocument().getText(new TextRange(start, end));
+                String line = editor.getDocument().getText(new TextRange(start, end)).length() == 0 ?
+                        editor.getDocument().getText(new TextRange(
+                                document.getLineStartOffset(logicalPosition.line) -2,
+                                document.getLineEndOffset(logicalPosition.line -2))):
+                        editor.getDocument().getText(new TextRange(start, end));
                 if(!line.equals(last)){
                     this.last = line;
                     System.out.println(line);
-                    try{
-                        GPTWrapper gptWrapper = new GPTWrapper(gptToken,PluginSettingsStateService.getInstance().getState().getLanguage());
 
-                        String response = gptWrapper.analyzeLine(line);
-                        Gson parser = new Gson();
-                        JsonObject object = parser.fromJson(response, JsonObject.class);
-                        VulnerabilityLine vulnerabilityLine = new VulnerabilityLine();
-                        vulnerabilityLine.setVulnerable(object.get("vulnerable").getAsString().equalsIgnoreCase("yes"));
-                        vulnerabilityLine.setDescription(object.get("description").getAsString());
-                        vulnerabilityLine.setSeverity(object.get("severity").getAsString());
+                    Thread t = new Thread(() -> {
+                        try{
+                            GPTWrapper gptWrapper = new GPTWrapper(gptToken,PluginSettingsStateService.getInstance().getState().getLanguage());
 
-                        if(vulnerabilityLine.isVulnerable()){
+                            String response = gptWrapper.analyzeLine(line);
+                            Gson parser = new Gson();
+                            JsonObject object = parser.fromJson(response, JsonObject.class);
+                            VulnerabilityLine vulnerabilityLine = new VulnerabilityLine();
+                            vulnerabilityLine.setVulnerable(object.get("vulnerable").getAsString().equalsIgnoreCase("yes"));
+                            vulnerabilityLine.setDescription(object.get("description").getAsString());
+                            vulnerabilityLine.setSeverity(object.get("severity").getAsString());
 
-                            NotificationGroupManager.getInstance().getNotificationGroup("vulnerabilityNotification")
-                                    .createNotification("Attento hai inserito del codice vulnerabile!",
-                                            "Riga: " + logicalPosition.line+"\n"+ vulnerabilityLine.getDescription(), NotificationType.INFORMATION).notify(project);
+                            if(vulnerabilityLine.isVulnerable()){
+                                PluginToolWindowLogWritingAnalysis toolWindowLogWritingAnalysis =
+                                        (PluginToolWindowLogWritingAnalysis) ToolWindowManager.getInstance(project)
+                                                .getToolWindow("DigitalTwinForCyberSecurity")
+                                                .getContentManager().getContent(1)
+                                                .getComponent();
+
+                                toolWindowLogWritingAnalysis.addVulnerabilityToContent(vulnerabilityLine,
+                                        FileEditorManager.getInstance(project), file.getVirtualFile());
+
+                                NotificationGroupManager.getInstance().getNotificationGroup("vulnerabilityNotification")
+                                        .createNotification("Attento hai inserito del codice vulnerabile!",
+                                                "Riga: " + logicalPosition.line+"\n"+
+                                                        vulnerabilityLine.getDescription(),
+                                                NotificationType.INFORMATION)
+                                        .addAction(ActionManager.getInstance().getAction("NotifyAction")).notify(project);
+                            }
+                        }catch (OpenAiHttpException exception){
+                            exception.printStackTrace();
                         }
-                    }catch (OpenAiHttpException exception){
-                        exception.printStackTrace();
-                    }
+                    });
+
+                    t.start();
 
                 }
 
--- src/main/java/it/unisa/zammarrelli/digitaltwinforcybersecurity/gui/PluginToolWindowFactory.java
+++ src/main/java/it/unisa/zammarrelli/digitaltwinforcybersecurity/gui/PluginToolWindowFactory.java
@@ -5,13 +5,21 @@ import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
+import it.unisa.zammarrelli.digitaltwinforcybersecurity.settings.PluginSettingsStateService;
 import org.jetbrains.annotations.NotNull;
 
 public class PluginToolWindowFactory implements ToolWindowFactory {
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-        PluginToolWindowContent toolWindowContent = new PluginToolWindowContent(true, false);
-        Content content = ContentFactory.getInstance().createContent(toolWindowContent, "", false);
-        toolWindow.getContentManager().addContent(content);
+        PluginToolWindowContentVulnerabilitiesAnalysis toolWindowContentVulnerabilitiesAnalysis = new PluginToolWindowContentVulnerabilitiesAnalysis(true, false);
+        Content contentVulnerabilitiesAnalysis = ContentFactory.getInstance().createContent(toolWindowContentVulnerabilitiesAnalysis, "Vulnerability Detector", false);
+        toolWindow.getContentManager().addContent(contentVulnerabilitiesAnalysis);
+
+        PluginToolWindowLogWritingAnalysis toolWindowLogWritingAnalysis = new PluginToolWindowLogWritingAnalysis(true, false);
+        Content contentWritingAnalysis = ContentFactory.getInstance().createContent(toolWindowLogWritingAnalysis, "Writing Analysis Log", false);
+        toolWindow.getContentManager().addContent(contentWritingAnalysis);
+
     }
+
+
 }
