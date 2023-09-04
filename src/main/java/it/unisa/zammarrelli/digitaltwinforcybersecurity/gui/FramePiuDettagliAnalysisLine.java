73474c283197f1e2daac79113c6da3a3295c6274
frankzamma <frank581.fgz@gmail.com>
2023-09-04 19:27:16 +0200

    Update grafica funzionalità analisi durante la scrittura

--- src/main/java/it/unisa/zammarrelli/digitaltwinforcybersecurity/gui/FramePiuDettagliAnalysisLine.java
+++ src/main/java/it/unisa/zammarrelli/digitaltwinforcybersecurity/gui/FramePiuDettagliAnalysisLine.java
@@ -5,6 +5,8 @@ import it.unisa.zammarrelli.digitaltwinforcybersecurity.common.VulnerabilityLine
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 
 public class FramePiuDettagliAnalysisLine extends JFrame {
     private VulnerabilityLine vulnerabilityLine;
@@ -18,7 +20,7 @@ public class FramePiuDettagliAnalysisLine extends JFrame {
         this.associatedPanel = panel;
         this.toolWindowLogWritingAnalysis = toolWindowLogWritingAnalysis;
         super.setLayout(new FlowLayout(FlowLayout.LEFT));
-        super.setLocationRelativeTo(null);
+
         super.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         createPanel();
@@ -26,31 +28,94 @@ public class FramePiuDettagliAnalysisLine extends JFrame {
 
 
     private void createPanel(){
-        JPanel panel = new JPanel(new GridLayout(4, 1));
+        JPanel panel = new JPanel(new GridBagLayout());
 
-        JPanel panelLine = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        panelLine.add(new JBLabel("Riga: " + vulnerabilityLine.getLine()));
-        panel.add(panelLine);
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridheight = 1;
+        constraints.gridwidth =3;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        JPanel panelFirst = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        panelFirst.add(new JBLabel("Riga: " + vulnerabilityLine.getLine()));
+        panelFirst.add(new JBLabel("Severity: " + vulnerabilityLine.getSeverity()));
+        panel.add(panelFirst, constraints);
 
-        JPanel severityPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        severityPanel.add(new JBLabel("Severity: " + vulnerabilityLine.getSeverity()));
-        panel.add(severityPanel);
-
+        constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.gridheight = 2;
+        constraints.gridwidth =4;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
         JPanel descriptionPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        descriptionPanel.add(new JBLabel("<html>"+vulnerabilityLine.getDescription()+"</html>"));
-        panel.add(descriptionPanel);
+        JBLabel descriptionLabel = new JBLabel("<html> Description<br>"
+                + vulnerabilityLine.getDescription().replaceAll("\\.", ".<br>")+  "</html>");
+        descriptionPanel.add(descriptionLabel);
+        panel.add(descriptionPanel,  constraints);
 
+        constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.gridheight = 2;
+        constraints.gridwidth = 4;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        JPanel solutionPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JBLabel solutionLabel = new JBLabel("<html>" +
+                "Solution: <br>" + vulnerabilityLine.getSolution().replaceAll("\\.", ".<br>")+
+                "</html>");
+        solutionLabel.setAllowAutoWrapping(true);
+        solutionPanel.add(solutionLabel);
+        panel.add(solutionPanel, constraints);
+
+        if(vulnerabilityLine.getExampleCode().length() > 0){
+            constraints = new GridBagConstraints();
+            constraints.gridx = 0;
+            constraints.gridy = 5;
+            constraints.gridheight = 3;
+            constraints.gridwidth = 4;
+            constraints.fill = GridBagConstraints.HORIZONTAL;
+            JPanel exampleCodePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            JBLabel exampleCodeLabel = new JBLabel( "Example code:<br>"+
+                    "<code>" + vulnerabilityLine.getExampleCode().replaceAll("\n", "<br>")  +
+                    "</code>");
+            exampleCodeLabel.setAllowAutoWrapping(true);
+            exampleCodeLabel.setCopyable(true);
+            exampleCodePanel.add(exampleCodeLabel);
+            panel.add(exampleCodeLabel, constraints);
+        }
+
+        constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = vulnerabilityLine.getExampleCode().length() == 0 ? 5 : 8;
+        constraints.gridheight = 1;
+        constraints.gridwidth = 4;
+        constraints.anchor = GridBagConstraints.LAST_LINE_START;
         JPanel panelButtons = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JButton eliminaButton = new JButton("Elimina");
-        JButton vediNelCodiceButton = new JButton("Visualizza nel codice");
 
-        panelButtons.add(vediNelCodiceButton);
+        if(vulnerabilityLine.getExampleCode().length() > 0){
+            JButton copiaCodiceButton = new JButton("Copia codice");
+            copiaCodiceButton.addActionListener(e ->{
+                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                clipboard.setContents(new StringSelection(vulnerabilityLine.getExampleCode()), null);
+                this.setVisible(false);
+            });
+            panelButtons.add(copiaCodiceButton);
+        }
+
+
+        JButton eliminaButton = new JButton("Elimina");
         panelButtons.add(eliminaButton);
 
-        vediNelCodiceButton.addActionListener(e ->this.setVisible(false));
-        eliminaButton.addActionListener(e->toolWindowLogWritingAnalysis.removeVulnerability(associatedPanel));
 
+        eliminaButton.addActionListener(e->{
+            toolWindowLogWritingAnalysis.removeVulnerability(associatedPanel);
+            this.setVisible(false);
+        });
+
+        panel.add(panelButtons,  constraints);
         super.add(panel);
         super.pack();
+        super.setLocationRelativeTo(null);
+        super.setResizable(false);
     }
 }
